7d5d67424b4777cc4077de7036feda0985a1c146
Enoch Julias <enochjul@users.noreply.github.com>
2017-09-07 02:51:55 +0800

    Add copy constructor and assignment operator to Bitset.

--- h/Bitset.h
+++ h/Bitset.h
@@ -30,27 +30,15 @@ private:
 	size_type Size;
 
 public:
+	Bitset() noexcept : Start(nullptr), Size(0) {
+	}
 	Bitset(size_type n, bool value = false) noexcept;
-
-	Bitset(const Bitset &) = delete;
-	Bitset &operator =(const Bitset &) = delete;
-
-	bool get(size_type pos) const noexcept {
-		assert(pos < Size);
-		return get(Start, pos);
+	~Bitset() noexcept {
+		Alloc::dealloc_array(Start, (Size + NUMBER_OF_BLOCK_TYPE_BITS - 1) / NUMBER_OF_BLOCK_TYPE_BITS);
 	}
 
-	void set(size_type pos) noexcept {
-		assert(pos < Size);
-
-		block_type *block = Start + (pos / NUMBER_OF_BLOCK_TYPE_BITS);
-		*block |= static_cast<block_type>(1) << (pos % NUMBER_OF_BLOCK_TYPE_BITS);
-	}
-
-	void set(size_type pos, bool value) noexcept {
-		assert(pos < Size);
-		set(Start, pos, value);
-	}
+	Bitset(const Bitset &a) noexcept;
+	Bitset &operator =(const Bitset &a) noexcept;
 
 	block_type *data() noexcept {
 		return Start;
@@ -60,11 +48,39 @@ public:
 		return Start;
 	}
 
-	static bool get(block_type *data, size_type pos) noexcept {
+	size_type size() const noexcept {
+		return Size;
+	}
+
+	bool empty() const noexcept {
+		return Size == 0;
+	}
+
+	bool get(size_type pos) const noexcept {
+		return get(Start, Size, pos);
+	}
+
+	void set(size_type pos) noexcept {
+		set(Start, Size, pos);
+	}
+
+	void set(size_type pos, bool value) noexcept {
+		set(Start, Size, pos, value);
+	}
+
+	static bool get(block_type *data, size_type n, size_type pos) noexcept {
+		assert(pos >= 0 && pos < n);
 		return (data[pos / NUMBER_OF_BLOCK_TYPE_BITS] & (static_cast<block_type>(1) << (pos % NUMBER_OF_BLOCK_TYPE_BITS))) != 0;
 	}
 
-	static void set(block_type *data, size_type pos, bool value) noexcept {
+	static void set(block_type *data, size_type n, size_type pos) noexcept {
+		assert(pos >= 0 && pos < n);
+		auto *block = data + (pos / NUMBER_OF_BLOCK_TYPE_BITS);
+		*block |= static_cast<block_type>(1) << (pos % NUMBER_OF_BLOCK_TYPE_BITS);
+	}
+
+	static void set(block_type *data, size_type n, size_type pos, bool value) noexcept {
+		assert(pos >= 0 && pos < n);
 		auto *block = data + (pos / NUMBER_OF_BLOCK_TYPE_BITS);
 		size_type mask = static_cast<block_type>(1) << (pos % NUMBER_OF_BLOCK_TYPE_BITS);
 		if (value) {
@@ -98,6 +114,40 @@ Bitset<T, Alloc>::Bitset(size_type n, bool value) noexcept {
 	} else {
 		Start = nullptr, Size = 0;
 	}
+}
+
+template <typename T, class Alloc>
+Bitset<T, Alloc>::Bitset(const Bitset &a) noexcept {
+	auto n = a.Size;
+	if (n > 0) {
+		auto number_of_blocks = (n + NUMBER_OF_BLOCK_TYPE_BITS - 1) / NUMBER_OF_BLOCK_TYPE_BITS;
+		auto *new_start = Alloc::template alloc_array<block_type>(number_of_blocks);
+		Start = new_start;
+		Size = n;
+		memcpy(new_start, a.Start, number_of_blocks * sizeof(block_type));
+	} else {
+		Start = nullptr, Size = 0;
+	}
+}
+
+template <typename T, class Alloc>
+auto Bitset<T, Alloc>::operator =(const Bitset &a) noexcept -> Bitset & {
+	auto old_n = Size, new_n = a.Size;
+	if (new_n != old_n) {
+		Alloc::dealloc_array(Start, (old_n + NUMBER_OF_BLOCK_TYPE_BITS - 1) / NUMBER_OF_BLOCK_TYPE_BITS);
+		if (new_n > 0) {
+			auto number_of_blocks = (new_n + NUMBER_OF_BLOCK_TYPE_BITS - 1) / NUMBER_OF_BLOCK_TYPE_BITS;
+			auto *new_start = Alloc::template alloc_array<block_type>(number_of_blocks);
+			Start = new_start;
+			Size = new_n;
+			memcpy(new_start, a.Start, ((new_n + NUMBER_OF_BLOCK_TYPE_BITS - 1) / NUMBER_OF_BLOCK_TYPE_BITS) * sizeof(block_type));
+		} else {
+			Start = nullptr, Size = 0;
+		}
+	} else {
+		memcpy(Start, a.Start, ((new_n + NUMBER_OF_BLOCK_TYPE_BITS - 1) / NUMBER_OF_BLOCK_TYPE_BITS) * sizeof(block_type));
+	}
+	return *this;
 }
 
 }
